6eaf8c7976034f5f9d22bd21656a59a849abeb32
volvo64 <33491829+volvo64@users.noreply.github.com>
2018-03-21 10:10:41 -0700

    Initial commit

---
+++ hostedExchange/dcg_hosted-exchange_create-mailbox.ps1
@@ -0,0 +1,55 @@
+﻿$firstName = Read-Host -Prompt 'First Name:'
+$lastName = Read-Host -Prompt 'Last Name:'
+$companies = Get-Content companies.txt
+
+# https://docs.microsoft.com/en-us/powershell/scripting/getting-started/cookbooks/selecting-items-from-a-list-box?view=powershell-6
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form 
+$form.Text = "Select a Company"
+$form.Size = New-Object System.Drawing.Size(300,200) 
+$form.StartPosition = "CenterScreen"
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Point(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = "OK"
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $OKButton
+$form.Controls.Add($OKButton)
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Point(150,120)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = "Cancel"
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $CancelButton
+$form.Controls.Add($CancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20) 
+$label.Size = New-Object System.Drawing.Size(280,20) 
+$label.Text = "Please select a computer:"
+$form.Controls.Add($label) 
+
+$listBox = New-Object System.Windows.Forms.ListBox 
+$listBox.Location = New-Object System.Drawing.Point(10,40) 
+$listBox.Size = New-Object System.Drawing.Size(260,20) 
+$listBox.Height = 80
+
+foreach ($c in $companies):
+    [void] $listBox.Items.Add($c)
+
+
+$form.Controls.Add($listBox) 
+
+$form.Topmost = $True
+
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $listBox.SelectedItem
+    $x
+}
