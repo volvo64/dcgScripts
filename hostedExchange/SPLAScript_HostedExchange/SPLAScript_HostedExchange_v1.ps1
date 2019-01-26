9ecfff8de6c17a8dec93d1532626b995a52ca11b
Damon <damon@damonbreeden.com>
2019-01-26 18:07:08 -0500

    fixed spla reports for hosted exchange
    two changes, first i fixed the original code that i inherited, then i rewrote the script to be much cleaner and efficient in v2

--- hostedExchange/SPLAScript_HostedExchange/SPLAScript_HostedExchange_v1.ps1
+++ hostedExchange/SPLAScript_HostedExchange/SPLAScript_HostedExchange_v1.ps1
@@ -1,32 +1,35 @@
-Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+ï»¿Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
 $MyDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $Month = Get-Date -UFormat %B
 $time = Get-Date
 $logfile = "$myDir\logs\logfile.log"
 Add-Content $logfile "Script ran on $(get-date -f yyyy-MM-dd)"
 
-$filterednames = @("mimecast","guest","LDAP","vmware","dss","opendns","sp admin","dcg","qbdataservice","sql","st_bernard","hosted","ldapadmin","spadmin","test","noc","st. bernard","st bernard","managed care","bbadmin","besadmin","compliance","discovery","rmmscan","healthmailbox","sharepoint","windows sbs","qbdata","noc_helpdesk","appassure","support","scanner","ftp","app assure","aspnet","Dependable Computer Guys","efax","exchange","INSTALR","IUSR","IWAM","NAV","Quick Books")
+$filterednames = @("dmarc","mimecast","guest","LDAP","vmware","dss","opendns","sp admin","dcg","qbdataservice","sql","st_bernard","hosted","ldapadmin","spadmin","test","noc","st. bernard","st bernard","managed care","bbadmin","besadmin","compliance","discovery","rmmscan","healthmailbox","sharepoint","windows sbs","qbdata","noc_helpdesk","appassure","support","scanner","ftp","app assure","aspnet","Dependable Computer Guys","efax","exchange","INSTALR","IUSR","IWAM","NAV","Quick Books")
 $regex = "(" + ($filterednames -join "|") + ")"
 Add-Content $logfile "Filtering out the following names: $filterednames"
 
-$PSEmailServer = "host-exch90.dcgla.com"
+$PSEmailServer = ".com"
 $SMTPPort = 2525
-$SMTPUsername = "scriptsender@dcgla.net"
-$EncryptedPasswordFile = "$mydir\scriptsender@dcgla.net.securestring"
+$SMTPUsername = ".net"
+$EncryptedPasswordFile = "$mydir\.net.securestring"
 $SecureStringPassword = Get-Content -Path $EncryptedPasswordFile | ConvertTo-SecureString
 $EmailCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $SMTPUsername,$SecureStringPassword
-$MailTo = "monitoring@dcgla.com"
-$MailFrom = "scriptsender@dcgla.net"
+$MailTo = ".com"
+$MailFrom = ".net"
 $mailAttachments = @()
 
 Add-Content $logfile 'Beginning search of Exchange mailboxes.'
 
-#Updated code to ignore AD accounts that are disabled, sorted by Name
-$UsersGroups = Get-Mailbox -Filter {(userAccountControl -ne '514') -AND (userAccountControl -ne '546') -AND (userAccountControl -ne '66050') `
-	-AND (userAccountControl -ne '66082')} | Group-Object -Property:OrganizationalUnit | Select-Object Name,Count
+#this first section simply creates the counts, it does not export anything by name.
 
-#Added to count shared mailboxes
-$UsersGroupsShared = Get-Mailbox -Filter {IsLinked -eq $true} | Group-Object -Property:OrganizationalUnit | Select-Object Name,Count
+#Updated code to ignore AD accounts that are disabled, sorted by Name
+$searchbase = "Hosted1.local/Hosting"
+$UsersGroups = Get-Mailbox -OrganizationalUnit $searchbase -Filter {(userAccountControl -ne '514') -AND (userAccountControl -ne '546') -AND (userAccountControl -ne '66050') `
+	-AND (userAccountControl -ne '66082')} | Where-Object {$_ -notmatch $regex} | Group-Object -Property:OrganizationalUnit
+
+#Added to count linked mailboxes
+$UsersGroupsShared = Get-Mailbox -OrganizationalUnit $searchbase -Filter {IsLinked -eq $true} | Where-Object {$_ -notmatch $regex} | Group-Object -Property:OrganizationalUnit 
 
 #Combine both arrays
 $UsersGroups = $UsersGroups += $UsersGroupsShared
@@ -35,16 +38,17 @@ $UsersGroups = $UsersGroups | Sort-Object Name
 #Array to hold Accounts Count data
 $UserAccountsC = @()
 
-#Loop through each user in the array
+#Loop through each OU in the array
 ForEach ($User in $UsersGroups)
 {
 	#Strip local domain name for easier reading
-	$User.Name = $User.Name -replace "hosted1.local/Hosting/", ""
+	$UserName = $User.Name -replace "hosted1.local/Hosting/", ""
 	
 	#Capture the user name if the User is in an OU with " - Users" in the name
 	If ($User.Name -match " - Users" -And $User.Name -notmatch "dcg")
 	{
-		$UserAccountsC = $UserAccountsC += $User
+		$clientcount = $User | select Name,Count
+        $UserAccountsC = $UserAccountsC += $clientcount
 	}
 }
 
@@ -53,24 +57,8 @@ $UserAccountsC | Export-CSV "$MyDir\logs\$(get-date -f yyyy-MM-dd)UserAccountCou
 $UserAccountsCountAttachment = "$MyDir\logs\$(get-date -f yyyy-MM-dd)UserAccountCount.csv"
 
 
+#This section goes through and actually compiles list of users and exports them
 
-
-#Updated code to ignore AD accounts that are disabled, sorted by Name
-$UsersGroups = Get-Mailbox -Filter {(userAccountControl -ne '514') -AND (userAccountControl -ne '546') -AND (userAccountControl -ne '66050') `
-	-AND (userAccountControl -ne '66082')} | Group-Object -Property:OrganizationalUnit | Select-Object Name,Group | Sort-Object Name
-
-#Added to count shared mailboxes
-$UsersGroupsShared = Get-Mailbox -Filter {IsLinked -eq $true} | Group-Object -Property:OrganizationalUnit | Select-Object Name,Group
-
-#$UsersGroupsShared
-#Read-Host 'Shared - Please press ENTER' | Out-Null
-
-#Combine both arrays
-$UsersGroups = $UsersGroups += $UsersGroupsShared
-$UsersGroups = $UsersGroups | Sort-Object Name
-
-#$UsersGroups
-#Read-Host 'All - Please press ENTER' | Out-Null
 
 #Array to hold Accounts Breakdown data
 $UserAccountsB = @()
@@ -78,17 +66,14 @@ $UserAccountsB = @()
 #Loop through each user in the array
 ForEach ($User in $UsersGroups)
 {
-	ForEach ($Name in $User.Group)
-		{ 
-		If ($Name.Name -notmatch "test" -And $Name.Name -notmatch "administrator" -And $Name.Name -notmatch "dcg")
-		{
+	ForEach ($Name in $User.Group)		{
 		#Create a new custom object to hold our result.
 		$UserObject = New-Object PSObject
 
 		#Strip local domain name for easier reading
-		$User.Name = $User.Name -replace "hosted1.local/Hosting/", ""
+		$UserName = $User.Name -replace "hosted1.local/Hosting/", ""
 		#Split Company Name and OU
-		$SplitString = $User.Name -split '/'
+		$SplitString = $UserName -split '/'
 		$Company = $SplitString[0]
 		$OU = $SplitString[1]
 
@@ -97,11 +82,11 @@ ForEach ($User in $UsersGroups)
 		$UserObject | add-member -membertype NoteProperty -name "OU" -Value $OU
 		$UserObject | add-member -membertype NoteProperty -name "User" -Value $Name.Name
 
-		#Save the current $UserObject by appending it to $UserAccountsB ( += means append a new element to ‘me’)
+		#Save the current $UserObject by appending it to $UserAccountsB ( += means append a new element to â€˜meâ€™)
 		$UserAccountsB += $UserObject
 		}
 		}
-}
+
 
 #Export the User Account Breakdown - information to a CSV file
 $UserAccountsB | Sort-Object -Property Company, User | Export-csv "$MyDir\logs\$(get-date -f yyyy-MM-dd)UserAccountsBreakdown.csv" -nti
@@ -109,7 +94,7 @@ $UserAccountsBreakdownAttachment = "$MyDir\logs\$(get-date -f yyyy-MM-dd)UserAcc
 
 Add-Content $logfile "Sending email now"
 
-$MailSubject = "$Month's DCG Hosted Exchange audit"
+$MailSubject = "$Month's  Hosted Exchange audit"
 $MailBody = "$Month's SPLA Audit Info
 
 Attached are the CSV files with the customers and number of Hosted Exchange accounts and the breakdown of the individual accounts that has the users for each company.
---
+++ hostedExchange/SPLAScript_HostedExchange/SPLAScript_HostedExchange_v2.ps1
@@ -0,0 +1,60 @@
+﻿Import-Module ActiveDirectory
+
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+. $env:ExchangeInstallPath\bin\RemoteExchange.ps1
+Connect-ExchangeServer -auto
+$MyDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$Month = Get-Date -UFormat %B
+$time = Get-Date
+$timestamp = get-date -f yyyy-MM-dd
+$logfile = "$myDir\logs\logfile.log"
+Add-Content $logfile "Script ran on $timestamp"
+
+$filterednames = @("dmarc","mimecast","guest","LDAP","vmware","dss","opendns","sp admin","dcg","qbdataservice","sql","st_bernard","hosted","ldapadmin","spadmin","test","noc","st. bernard","st bernard","managed care","bbadmin","besadmin","compliance","discovery","rmmscan","healthmailbox","sharepoint","windows sbs","qbdata","noc_helpdesk","appassure","support","scanner","ftp","app assure","aspnet","Dependable Computer Guys","efax","exchange","INSTALR","IUSR","IWAM","NAV","Quick Books")
+$regex = "(" + ($filterednames -join "|") + ")"
+Add-Content $logfile "Filtering out the following names: $filterednames"
+
+$PSEmailServer = "mail.server.com"
+$SMTPPort = 2525
+$SMTPUsername = "username@domain.com"
+$EncryptedPasswordFile = "$mydir\email.securestring"
+$SecureStringPassword = Get-Content -Path $EncryptedPasswordFile | ConvertTo-SecureString
+$EmailCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $SMTPUsername,$SecureStringPassword
+$MailTo = "mailto@domain.com"
+$MailFrom = "mailfrom@domain.com"
+$mailAttachments = @()
+$dbcountAttachment = "$MyDir\logs\$timestamp`_UsersCount.csv"
+$dbMailboxesAttachment = "$MyDir\logs\$timestamp`_UserBreakdown.csv"
+
+Add-Content $logfile 'Beginning search of Exchange mailboxes.'
+
+$databases = Get-MailboxDatabase | Sort-Object Name
+
+foreach ($d in $databases) {
+    $dbmailboxes = Get-Mailbox -Database $d -RecipientTypeDetails UserMailbox,LinkedMailbox  | Where-Object {$_ -notmatch $regex}
+    $dbcount = $dbmailboxes.count
+    If ($dbcount -ne 0) {
+        $CountExport = New-Object PSObject
+        $CountExport | Add-Member -MemberType NoteProperty -Name "Database" -Value $d.Name
+        $CountExport | Add-Member -MemberType NoteProperty -Name "Count" -Value $dbcount
+        $CountExport | Export-Csv $dbcountAttachment -Append -NoTypeInformation
+        $dbmailboxes | Select-Object Database,Name,PrimarySmtpAddress,CustomAttribute1,Organizationalunit | 
+            Sort-Object Name | Export-Csv $dbMailboxesAttachment -Append -NoTypeInformation
+        }
+    }
+
+
+Add-Content $logfile "Sending email now"
+
+$MailSubject = "$Month's  Hosted Exchange audit"
+$MailBody = "$Month's SPLA Audit Info
+
+Attached are the CSV files with the customers and number of Hosted Exchange accounts and the breakdown of the individual accounts that has the users for each company.
+
+This message was sent from $env:COMPUTERNAME"
+
+$mailAttachments = $mailAttachments += $dbcountAttachment
+$mailAttachments = $mailAttachments += $dbMailboxesAttachment
+
+Send-MailMessage -From $MailFrom -To $MailTo -Subject $MailSubject -Body $MailBody -Port $SMTPPort -Credential $EmailCredential -Attachments $mailAttachments
+
