2c57cc389c660225c6c246d3e88b30c2909b4327
Damon Breeden <49255023+damonbreeden@users.noreply.github.com>
2020-01-16 11:54:51 -0500

    pulled spla

--- SPLA/Generate-Password-File.ps1
+++
@@ -1,3 +0,0 @@
-﻿$MyDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$EncryptedPasswordFile = "$mydir\scriptsender@dcgla.net.securestring"
-Read-Host "Type the password for scriptsender@dcgla" -AsSecureString | ConvertFrom-SecureString | Out-File -FilePath $EncryptedPasswordFile
--- SPLA/Run-SPLA-Script.ps1
+++
@@ -1,3 +0,0 @@
-﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/volvo64/dcgScripts/master/SPLA/SPLAScriptv1.ps1
-Invoke-Expression $($ScriptFromGitHub.Content)
